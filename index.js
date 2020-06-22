3fb8becb07c8ca328d85142fb9d44bad29f0b9ad
PshenichnyiKostya <kpshenichnyi@bk.ru>
2020-06-22 15:01:59 +0300

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.use(cors());
 app.use('/v1/auth/', authRouter)
 app.use('/v1/data/jog/', jogRouter)
 
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("build"));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    });
+}
+
 async function start() {
     try {
         await mongoose.connect("mongodb+srv://kostya:123@jog-4efdr.mongodb.net/jog?retryWrites=true&w=majority", {
@@ -32,7 +42,7 @@ async function start() {
             useCreateIndex: true,
             useFindAndModify: false
         })
-        app.listen(process.env.PORT || 5000),() => console.log('Server start')
+        app.listen(process.env.PORT || 5000), () => console.log('Server start')
 
     } catch (e) {
         console.error(e.message)
