62cc6825bd92275250cb8ca5a543fcee697365f7
PshenichnyiKostya <kpshenichnyi@bk.ru>
2020-06-23 14:01:09 +0300

    first commit

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const jogRouter = require('./routes/jogRouter')
 configurePassport(passport)
 const app = express()
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(bodyParser.json());
 app.use(passport.initialize());
 // app.use(logger('dev'));
