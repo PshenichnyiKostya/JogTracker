ccebc0f1b199bc7fbca8909069b1b14dfdce5212
PshenichnyiKostya <kpshenichnyi@bk.ru>
2020-10-04 15:50:00 +0300

    Deploy by docker

--- server/index.js
+++ server/index.js
@@ -2,37 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const passport = require("passport");
 const configurePassport = require("./middlewares/passport")
-const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
-const cors = require("cors")
-const path = require('path')
-// const logger = require("morgan")
 const authRouter = require('./routes/authRouter')
 const jogRouter = require('./routes/jogRouter')
 
 configurePassport(passport)
 const app = express()
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(express.json());
+app.get('/', (req, res) => res.send('Hello World'))
+app.get('/test', (req, res) => res.send({qwe:123}))
 app.use(express.urlencoded({extended: true}));
-// app.use(logger('dev'));
-app.use(cors());
 app.use('/v1/auth/', authRouter)
 app.use('/v1/data/jog/', jogRouter)
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 
 async function start() {
     try {
